f96f94611d5127a5cf6b2dc9d46261a36763fc6d
saeedgholamrezaee <saeed_gholamrezaee@live.com>
2025-03-20 15:02:48 +0330

    folder views commited

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-const server = require('express')
-const app = new server()
+const express = require('express');
+const app = express();
+const greet = express.Router();
+const bye = express.Router();
 
-app.listen(6000, function(err){
-    console.log('res method')
-})
+app.set('view engine', 'ejs');
+app.use('/greet', greet);
+app.use('/bye', bye);
+
+greet.get('/hi', (req, res, next) => {
+    res.render('ui/hi');
+});
+
+bye.get('/seeyou', (req, res, next) => {
+    res.render('ui/seeyou');
+});
 
 app.get('/', (req, res) => {
-    res.send('hell o')
-})
+    let numip = req.ip;
+    res.send(`<h1>node express js ${numip} this is</h1>`);
+});
+
+app.get('/page', (req, res) => {
+    res.render('home');
+});
+
+
+app.listen(6100, function(err){
+    if(err) console.log(err);
+});
+
