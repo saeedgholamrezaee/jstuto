de5b89ba2ce1d52b8cd71459dca1a9e1bdc8f660
saeedgholamrezaee <saeed_gholamrezaee@live.com>
2025-03-20 19:55:01 +0330

    commit to staged with change and add and delete

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const greet = express.Router();
 const bye = express.Router();
+const path = require("path");
 
-app.set('view engine', 'ejs');
-app.use('/greet', greet);
-app.use('/bye', bye);
+app.set("view engine", "ejs");
 
-greet.get('/hi', (req, res, next) => {
-    res.render('ui/hi');
+const data = {
+  fname: "saeed",
+  age: 48,
+  degree: 18.75,
+};
+
+greet.get("/", (req, res, next) => {
+  res.render("ui/hi");
 });
 
-bye.get('/seeyou', (req, res, next) => {
-    res.render('ui/seeyou');
+bye.get("/", (req, res, next) => {
+  res.render("ui/sy");
 });
 
-app.get('/', (req, res) => {
-    let numip = req.ip;
-    res.send(`<h1>node express js ${numip} this is</h1>`);
+app.get("/", (req, res) => {
+  let numip = req.ip;
+  res.send(
+    `<h1>node ${__dirname} data is ${data.fname} express js ${numip} this is</h1>`,
+  );
 });
 
-app.get('/page', (req, res) => {
-    res.render('home');
+app.get("/page", (req, res) => {
+  res.render("home");
 });
 
+app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(6100, function(err){
-    if(err) console.log(err);
+app.use("/greet", greet);
+app.use("/bye", bye);
+
+app.listen(6100, function (err) {
+  if (err) console.log(err);
 });
-
